753a5eaaede06d3bde85deb5f920d2282cff3915
Paulo Pereira <prpereiras89@gmail.com>
2021-10-25 16:21:05 -0300

    deploy dsc dashboard

--- django_zammad/settings.py
+++ django_zammad/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5^pu%b$^awn8aal#ne%exbq-mwa=00%zqizg#%%maql*!+i0qa'
+#SECRET_KEY = 'django-insecure-5^pu%b$^awn8aal#ne%exbq-mwa=00%zqizg#%%maql*!+i0qa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 #DEBUG = False
 
-#ALLOWED_HOSTS = ['www.dsci-dashboard.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.dsc-zammad.herokuapp.com']
+#ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -212,8 +212,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-""" 
+ 
 import django_heroku
 django_heroku.settings(locals())
-
- """
