c4eb0ebb95c0c20ab8f8138abe8fd7258d06fae0
Paulo Pereira <prpereiras89@gmail.com>
2021-10-29 11:09:46 -0300

    [update] dcc interval

--- django_zammad/settings.py
+++ django_zammad/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-5^pu%b$^awn8aal#ne%exbq-mwa=00%zqizg#%%maql*!+i0qa'
+SECRET_KEY = 'django-insecure-5^pu%b$^awn8aal#ne%exbq-mwa=00%zqizg#%%maql*!+i0qa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = ['www.dsc-zammad.herokuapp.com']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['www.dsc-zammad.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
